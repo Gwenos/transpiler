3797490b1f481fc8b6593c9dea533c29e17eadcb
Gwenos <srpgwen@gmail.com>
2025-08-25 11:02:11 +0200

    suppression class SyntacticAnalysis.java + modification test.py

--- ressources/test.py
+++ ressources/test.py
@@ -1,26 +1,24 @@
 a= 5
 b=2.3
-
 str="hello"
-
-
 c = ( str + popo )
 d = a + b - 7.3
-
 test = 1 + 4 * (1+1) / fonctionCallInExpression(arg)
-
 1 + 1
-
 fonctionCallOutExpression(arg1, arg2, arg3)
-
-#sans argument
+# #sans argument
 fonctionCallOutExpression()
-
-True
-
 # print()
-#
 # print(d)
+# True
+if 1 + 1 == 5 :
+if a + b > 3 and not done:
+if caca and crotte :
+if 1 + 1 > 12 and caca != crotte or crotte == rien :
+if 1 + 1 >= 5 or 2 + 2 != 4 :
+# if True and False :
+
+while maman and not papa==Nul :
 
 
 # def fonctionCall(arg1, arg2):
--- src/SyntacticAnalysis.java
+++
@@ -1,6 +0,0 @@
-public class SyntacticAnalysis {
-
-	public SyntacticAnalysis() {
-
-	}
-}
